1270ca1be6926a405a0dc92b45c129d9a26dbd72
KIMSEONGHA2223 <149854799+KIMSEONGHA2223@users.noreply.github.com>
2025-05-12 23:16:44 +0900

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -20,8 +20,8 @@ alias cat='bat '
 " >> $PREFIX/etc/bash.bashrc
 
 #Download Wallpaper
-wget https://raw.githubusercontent.com/yanghoeg/Termux_XFCE/main/dark_waves.png
-wget https://raw.githubusercontent.com/yanghoeg/Termux_XFCE/main/TheSolarSystem.jpg
+wget https://raw.githubusercontent.com/KIMSEONGHA2223/Termux_edit/main/dark_waves.png
+wget https://raw.githubusercontent.com/KIMSEONGHA2223/Termux_edit/main/TheSolarSystem.jpg
 mv dark_waves.png $PREFIX/share/backgrounds/xfce/
 mv TheSolarSystem.jpg $PREFIX/share/backgrounds/xfce/
 
@@ -56,21 +56,21 @@ rm Meslo.zip
 rm LICENSE.txt
 rm readme.md
 
-wget https://github.com/yanghoeg/Termux_XFCE/raw/main/NotoColorEmoji-Regular.ttf
+wget https://github.com/KIMSEONGHA2223/Termux_edit/raw/main/NotoColorEmoji-Regular.ttf
 mv NotoColorEmoji-Regular.ttf .fonts
 
-wget https://github.com/yanghoeg/Termux_XFCE/raw/main/font.ttf
+wget https://github.com/KIMSEONGHA2223/Termux_edit/raw/main/font.ttf
 mv font.ttf .termux/font.ttf
 
 #Setup Fancybash Termux
-wget https://raw.githubusercontent.com/yanghoeg/Termux_XFCE/main/fancybash.sh
+wget https://raw.githubusercontent.com/KIMSEONGHA2223/Termux_edit/main/fancybash.sh
 mv fancybash.sh .fancybash.sh
 echo "source $HOME/.fancybash.sh" >> $PREFIX/etc/bash.bashrc
 sed -i "326s/\\\u/$username/" $HOME/.fancybash.sh
 sed -i "327s/\\\h/termux/" $HOME/.fancybash.sh
 
 #Autostart Conky and Flameshot
-wget https://github.com/yanghoeg/Termux_XFCE/raw/main/config.tar.gz
+wget https://github.com/KIMSEONGHA2223/Termux_edit/raw/main/config.tar.gz
 tar -xvzf config.tar.gz
 rm config.tar.gz
 chmod +x .config/autostart/conky.desktop
